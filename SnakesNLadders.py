2849d1a229b2d6bacb35664ff736b476d72d7aaf
discipleofcoffee <kchawla.85@gmail.com>
2015-09-22 00:40:16 -0700

    Add comments and remove unused code
    - UnuedCode Class removed.
    - Dice.rollDice().rolledNumbers is now a list.
    - Doc strings added to all functions.

--- SnakesNLadders.py
+++ SnakesNLadders.py
@@ -6,105 +6,6 @@ possible classes: dice,turn,player(incl score and turn)
 import random
 import sys
 
-
-def unused():
-    pass
-
-    # def setPlayers(self, playerCount = 0):
-    #    ''' Creates the players.
-
-    #    Keyword args:
-    #    playerCount -- (default: 0, -- User Input) Number of Players.
-
-    #    Return: Dict
-    #    '''
-    #    self.playerList = {}
-    #    if playerCount != 0:
-    #        self.playerCount = playerCount
-    #    else:
-    #        self.playerCount = int(input('How many are playing? '))
-    #    for idx in range(1,self.playerCount+1): 
-    #        print('Enter name of player ', idx, ':')
-    #        playerNameTemp = input()
-    #        self.playerList[idx] = playerNameTemp
-    #    return(self.playerList)
-
-
-
-    # def playerTokens(self):
-    #    playerTokenPosition = {}
-    #    for idx in range(1,self.maxTokens+1):
-    #        playerTokenPosition = {idx,playerTokenPosition}
-
-
-    # class Board(object):
-    #    ''' #Specifies the attributes of the board.
-    #    '''
-
-    #    boardLayout = {1:38, 
-    #                   4:14, 
-    #                   9:31, 
-    #                   17:7, 
-    #                   21:42, 
-    #                   28:84, 
-    #                   51:67, 
-    #                   54:34, 
-    #                   62:19, 
-    #                   64:60, 
-    #                   71:91,
-    #                   80:100, 
-    #                   87:24, 
-    #                   93:73, 
-    #                   95:75, 
-    #                   98:79}
-
-
-
-
-
-
-    # def startCheck(rolledNumbers,gateValue=[6]):
-    #        ''' Checks if the player has a rolled the gating values before they can move their token.
-
-    #        Keyword args:
-    #        rolledNumbers -- accepts the numbers rolled by the dice in a player's turn.
-    #        gateValue -- specifies the value(s) the player needs to roll to begin play. Default is 6.
-
-    #        Returns:boolean
-    #        '''
-
-    #        playerOpen = rolledNumbers in gateValue
-    #        return(playerOpen)
-
-
-
-
-    # rolledNums = rollDice(getDice())
-    # print(rolledNums)
-    # print(startCheck(rolledNums))
-
-    # gameKeeper = GameKeeper()
-    # print(gameKeeper.setPlayers())
-
-    # dice = Dice(customDice=0,diceCount=1, diceSides=6)
-    # print(startCheck((dice.rollDice())))
-
-    # MAXPOSITION = 100
-    # createDice(sides,number)
-    # playerCount = input('Enter number of players: ')
-    # for loopCounter in range(1,playerCount):
-    #    print('Enter player %d name:', loopCounter)
-    #    playerNameTemp = input()
-    #    setPlayers(playerNameTemp,playerPosition=0)
-
-    # for turnCounter in range(1,MAXPOSITION):
-    #    for playerCounter in range(1,playerCount):    
-    #        getPlayer(playerCount)
-    #        newPosition = rollDie()
-    #        movePlayer(checkBoard(newPosition))
-    pass
-
-
 class Dice(object):
     """ Specifies the attributes required for the dice.
 
@@ -118,7 +19,7 @@ class Dice(object):
     def __init__(self, customDice=0, diceCount=1, diceSides=6):
         """ Initializes the parameters of the dice- how many dice, how many sides.
 
-        returns: none
+        :return: none
         """
 
         if customDice != 0:
@@ -128,7 +29,6 @@ class Dice(object):
         try:
             # abs(int(float(diceCount): ensures any value entered is converted to appropriate number.
             # Ex - float('-6.7') --> -6.7, int(-6.7) --> -6, abs(-6) --> 6.
-
             diceCount = abs(int(float(diceCount)))
         except ValueError:
             # raised if user input can not be converted into a numerical value. Ex- 'a' .
@@ -154,31 +54,31 @@ class Dice(object):
         self.diceSides = diceSides
 
     def rollDice(self):
-        """Rolls the dice for one turn.
+        """Rolls the dice for one turn. Returns the sum of the dice values.
 
-        Keyword args:
-        diceSides -- number of sides of the dice. Default is 6.
-        diceCount -- number of dice rolled together. Default is 1.
-
-        Returns: list of int
+        :return: int
         """
+        rolledNumbers = []
+        for idx in range(1,self.diceCount+1):
+            rolledNumbers.append(random.randint(1, self.diceSides+1))
 
-        # Sampling 'diceCount' number of values from population 1:diceSides without replacement.
-        # How well does it work for 2 dice? Maybe I should use sets for each die.
-        rolledNumbers = random.sample(range(1, self.diceSides + 1), self.diceCount)
-        return (sum(rolledNumbers))
+        return sum(rolledNumbers)
 
 
 class GameMaster(object):
-    """ Setsup and runs the game.
+    """ Gets player count, checks if a player has won, moves the player along snakes and ladders on the board.
     """
 
     def __init__(self):
 
-        self.Player = Player()
-        self.Dice = Dice()
+        self.Player = Player() # Plyaer class imported using composition.
+        self.Dice = Dice()  # Dice class imported using comprehension.
 
     def getPlayerCount(self):
+        """ Prompts the user to enter the number of players.
+
+        :return: int
+        """
         playerCount = input('Enter number of players: ')
         try:
             # abs(int(float(playerCount): ensures any value entered is converted to appropriate number.
@@ -186,20 +86,23 @@ class GameMaster(object):
             playerCount = abs(int(float(playerCount)))
         except ValueError:
             # raised if user input can not be converted into a numerical value. Ex- 'a' .
-            print(
-                'Invalid value. Number of players can only be an integer with value 1 or more. Using default value of 1.')
+            print('Invalid value. Number of players can only be an integer with value 1 or more. Using default value of 1.')
             playerCount = 1
         except NameError:
             # raised if user input is a character which the program deals with as a variable. Ex- a .
-            print(
-                'Invalid value. Number of players can only be an integer with value 1 or more. Using default value of 1.')
+            print('Invalid value. Number of players can only be an integer with value 1 or more. Using default value of 1.')
             playerCount = 1
-        return (playerCount)
+        return playerCount
 
     def winCheck(self, currentPlayerOutcome, currentPlayerName):
-        """
+        """ Checks if a player has reached the winning position.
+        Prints the win message and terminates program.
 
-        :rtype : int
+        Keyword args:
+            currentPlayerOutcome --  Accepts an int value specific to each player. 1 indicates player has won.
+            currentPLayerName -- Accepts a str indicating the player's name
+
+        :return : none
         """
         if currentPlayerOutcome == 1:
             print('Player', currentPlayerName, 'won!')
@@ -208,6 +111,11 @@ class GameMaster(object):
             sys.exit()
 
     def snlReposition(self, currentPlayerPosition):
+        """ Checks if a player is at a ladder or snake head and repositions them.
+        
+        :param currentPlayerPosition: Accepts an int value indicating a specific player's position
+        :return: int
+        """
         boardLayout = {1: 38,
                        4: 14,
                        9: 31,
@@ -238,37 +146,53 @@ class Player(object):
     def __init__(self, playerName='', playerPosition=0):
         """ Initializes parameters of the player.
 
-        Keyword args:
-        playerName -- (default: '') Specifies plaeyr name.
-        playerPosition -- (default: 0) Specifies score of player. STarting score is 0.
+       :param playerName: (default: '') Specifies plaeyr name.
+        :param playerPosition:(default: 0) Specifies score of player. STarting score is 0.
+
+        :return: none
         """
         self.playerName = playerName
         self.playerPosition = playerPosition
         self.playerWon = 0
         self.Dice = Dice()
 
-    def rollDice(self):
+    def rollDice(self): #Die.rollDie() imported into Player using composition.
+        """ Rolls the dice for one turn. Returns the sum of the dice values.
+
+        :return: int
+        """
         return (self.Dice.rollDice())
 
     def getPlayer(self, playerQuery='position'):
-        if (playerQuery == 'name' or playerQuery == 'Name' or playerQuery == 'NAME'):
-            return (self.playerName)
-        elif (playerQuery == 'position' or playerQuery == 'Position' or playerQuery == 'POSITION'):
-            return (self.playerPosition)
-        elif (playerQuery == 'outcome' or playerQuery == 'Outcome' or playerQuery == 'OUTCOME'):
+        """ Returns player related data.
+
+        :param playerQuery: 'name' | 'position' | 'outcome'
+        :return: string | int | int
+        """
+
+        playerQuery = playerQuery.lower()
+        if playerQuery == 'name':
+            return self.playerName
+        elif playerQuery == 'position':
+            return self.playerPosition
+        elif playerQuery == 'outcome':
             return (self.playerWon)
 
     def movePlayer(self, newPosition):
-        if (self.playerPosition == 0 and newPosition != 6):
+        """ Checks if player object is eligible to move and Changes the position of the player object.
+        :param newPosition: int
+        :return: none
+        """
+        if self.playerPosition == 0 and newPosition != 6:
             print('Player', self.playerName, 'cannot move until they roll a 6. They rolled a', newPosition,
                   '. Turn to next player.')
-        elif (self.playerPosition == 0 and newPosition == 6):
+        elif self.playerPosition == 0 and newPosition == 6:
             newPosition = Player.rollDice(self)
             self.playerPosition = newPosition
             print('We have a 6! And', self.playerName, 'moves to', self.playerPosition, '.')
         else:
             self.playerPosition = newPosition
-            if (self.playerPosition > MAXPOSITION):
+            if self.playerPosition > MAXPOSITION:
                 self.playerWon = 1
             else:
                 print('', self.playerName, 'moves to', self.playerPosition, '.')
@@ -281,6 +205,7 @@ if __name__ == '__main__':
     game = GameMaster()
     playerCount = game.getPlayerCount()
     allPlayers = []
+    # loop to get player names and initialize positions to 0.
     for playerCounter in range(0, playerCount):
         print('Enter player', playerCounter + 1, 'name:')
         playerNameTemp = input()
